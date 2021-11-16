5eda731c3c53c56527a167a41fa157c61921399b
Marc Sednaoui <marcsednaoui@hotmail.com>
2021-11-16 13:22:00 +0100

    add avatar to artisit profile and replace boostrap cards with ant

--- packages/react-app/pages/artistProfile.js
+++ packages/react-app/pages/artistProfile.js
@@ -1,12 +1,15 @@
 import React, { useContext, useState } from "react";
 import { Web3Context } from "../helpers/Web3Context";
 import axios from "axios";
-import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Row, Col, Card, Image, Avatar } from "antd";
+import utilStyles from "../styles/utils.module.css";
 
 // TODO: get artist address from previous page of list of artists
-const tempArtistAddress = "";
+// TODO: get artisit ENS avatar and name if available
+const artisitAvatarURL =
+  "https://storage.googleapis.com/sentinel-nft/raw-assets/c_0x72b6dc1003e154ac71c76d3795a3829cfd5e33b9_t_3270_raw_asset.jpeg";
+
+const artistName = "Marc";
 
 // your NFT_PORT_AUTH token here..
 const NFT_PORT_AUTH = "";
@@ -29,6 +32,8 @@ function artistProfile() {
   };
   const [cards, setCards] = useState([]);
 
+  const { Meta } = Card;
+
   axios
     .request(options)
     .then(function (response) {
@@ -37,13 +42,9 @@ function artistProfile() {
       const nftCards = nfts.map(nft => {
         if (nft.cached_file_url) {
           return (
-            <Col key={nft.token_id}>
-              <Card key={nft.token_id} style={{ width: "18rem" }}>
-                <Card.Title>{nft.name}</Card.Title>
-                <Card.Img variant="top" src={nft.cached_file_url} />
-                <Card.Body>
-                  <Card.Text>{nft.description}</Card.Text>
-                </Card.Body>
+            <Col span={8} key={nft.token_id}>
+              <Card key={nft.token_id} style={{ width: "25rem" }} cover={<Image src={nft.cached_file_url} />}>
+                <Meta title={nft.name} description={nft.description} />
               </Card>
             </Col>
           );
@@ -59,9 +60,11 @@ function artistProfile() {
   return (
     <div className="flex flex-1 flex-col h-screen w-full items-center">
       <div className="text-center" style={{ margin: 64 }}>
-        <p>Marc {tempArtistAddress}</p>
+        <Avatar src={<Image src={artisitAvatarURL} />} size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }} />
+        <h1 className={utilStyles.headingXl}>{artistName}</h1>
+        <p>{tempArtistAddress}</p>
       </div>
-      <Row>{cards}</Row>
+      <Row gutter={16}>{cards}</Row>
       <div className="text-center"></div>
     </div>
   );
