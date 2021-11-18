2101c74b43f55c5e802b4a23d7bcc3ba030a3761
Marc Sednaoui <marcsednaoui@hotmail.com>
2021-11-18 17:12:50 +0100

    add confirmation modal after minting NFT

--- packages/react-app/components/MintingFileForm.jsx
+++ packages/react-app/components/MintingFileForm.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, Modal, Space, Row, Col } from "antd";
 import { easyMintingWithFileUpload } from "../helpers/nftPortAPI";
+import { CheckCircleOutlined } from "@ant-design/icons";
+import { Web3Context } from "../helpers/Web3Context";
 
 const MintingFileForm = () => {
-  const onFinish = values => {
-    easyMintingWithFileUpload("rinkeby", values.title, values.description, values.wallet_address, myFile);
+  const web3 = React.useContext(Web3Context);
+
+  const [isModalVisible, setIsModalVisible] = React.useState(false);
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const [tx, setTX] = React.useState({ name: "", description: "", transaction_external_url: "" });
+
+  const onFinish = async values => {
+    setLoading(true);
+    const response = await easyMintingWithFileUpload("rinkeby", values.title, values.description, web3.address, myFile);
+
+    setIsModalVisible(true);
+    setTX(response);
+    setLoading(false);
   };
 
   const onFinishFailed = errorInfo => {
     console.log("Failed:", errorInfo);
   };
+
+  const [loading, setLoading] = React.useState(false);
 
   const [form] = Form.useForm();
 
@@ -53,21 +72,35 @@ const MintingFileForm = () => {
         />
       </Form.Item>
 
-      <Form.Item
-        name="wallet_address"
-        label="Address"
-        type="text"
-        required
-        rules={[{ required: true, message: "add an address to mint to" }]}
-      >
-        <Input placeholder="0x3123123ksad98asd98asd89jknjk" />
-      </Form.Item>
-
       <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
+        <Button type="primary" htmlType="submit" loading={loading}>
           Disegno NFT
         </Button>
       </Form.Item>
+
+      <Modal
+        visible={isModalVisible}
+        title="Your NFT was Created!"
+        onCancel={handleCancel}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Close
+          </Button>,
+        ]}
+      >
+        <Space className="flex-col">
+          <Row>{tx.name === "undefined" ? "Nameless" : tx.name}</Row>
+          <Row>{tx.description === "undefined" ? "No Description" : tx.description}</Row>
+          <Row>
+            <Col span={20}>
+              <a href={tx.transaction_external_url}>{tx.transaction_external_url}</a>
+            </Col>
+            <Col>
+              <CheckCircleOutlined twoToneColor="#52c41a" />
+            </Col>
+          </Row>
+        </Space>
+      </Modal>
     </Form>
   );
 };
